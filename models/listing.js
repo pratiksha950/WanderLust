998baae43084d769e423db38b1eb7ca18f13aa19
pratiksha950 <salunkepratiksha950@gmail.com>
2026-01-20 07:04:00 +0530

    initialized database done:

--- models/listing.js
+++ models/listing.js
@@ -1,23 +1,71 @@
-const mongoose =require("mongoose")
-const Schema=mongoose.Schema;
+// const mongoose =require("mongoose")
+// const Schema=mongoose.Schema;
 
-const listingSchema=new Schema({
-    title:{
-        type:String,
-        required:true
+// const listingSchema=new Schema({
+//     title:{
+//         type:String,
+//         required:true
+//     },
+//     description:String,
+//     image:{
+//         default:"https://unsplash.com/photos/a-painting-of-a-river-surrounded-by-pink-flowers-ZWg3KmVdtVw",
+//         type:String,
+//         set:(v)=>v ===""?"https://unsplash.com/photos/a-painting-of-a-river-surrounded-by-pink-flowers-ZWg3KmVdtVw":v,
+//     },
+//     price:Number,
+//     location:String,
+//     country:String
+// })
+
+// //collection
+// const Listing=mongoose.model("Listing",listingSchema);
+
+// module.exports=Listing;
+
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+
+  description: {
+    type: String,
+    required: true,
+  },
+
+  image: {
+    filename: {
+      type: String,
+      default: "listingimage",
     },
-    description:String,
-    image:{
-        default:"https://unsplash.com/photos/a-painting-of-a-river-surrounded-by-pink-flowers-ZWg3KmVdtVw",
-        type:String,
-        set:(v)=>v ===""?"https://unsplash.com/photos/a-painting-of-a-river-surrounded-by-pink-flowers-ZWg3KmVdtVw":v,
+    url: {
+      type: String,
+      required: true,
     },
-    price:Number,
-    location:String,
-    country:String
-})
+  },
 
-//collection
-const Listing=mongoose.model("Listing",listingSchema);
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
 
-module.exports=Listing;
+  location: {
+    type: String,
+    required: true,
+  },
+
+  country: {
+    type: String,
+    required: true,
+  },
+});
+
+// collection
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
