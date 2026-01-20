02a8b734c07a87b61b521a05639456b2d542c0cc
pratiksha950 <salunkepratiksha950@gmail.com>
2026-01-20 12:12:54 +0530

    update functionality added:

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
 const Listing =require("./models/Listing.js");
-const path=require("path")
+const path=require("path");
+const methodOverride=require("method-override")
 //database  
 const MONGO_URL="mongodb://127.0.0.1:27017/WanderLust";
 
@@ -19,6 +20,7 @@ async function main() {
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"))
 
 
 //index routev completed
@@ -28,7 +30,7 @@ app.get("/listings",async(req,res)=>{
     });
 
 //new route
-app.get("/listing/new",(req,res)=>{
+app.get("/listings/new",(req,res)=>{
     res.render("listings/new")
 })
 
@@ -40,10 +42,24 @@ app.get("/listings/:id",async (req,res)=>{
 })
 
 //create Route
-app.post("/listing",async (req,res)=>{
+app.post("/listings",async (req,res)=>{
     const newListing=new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings")
+})
+
+//Edit Route
+app.get("/listings/:id/edit",async(req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/edit",{listing})
+})
+
+//update Route
+app.put("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing})
+    res.redirect(`/listings/${id}`)
 })
 
 // app.get("/testListing",async(req,res)=>{
