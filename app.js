197c430b2a85f6d3d0ef46cefb7138c5052a4c4d
pratiksha950 <salunkepratiksha950@gmail.com>
2026-01-20 07:35:55 +0530

    index routev completed:

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
-const Listing =require("./models/Listing.js")
-
+const Listing =require("./models/Listing.js");
+const path=require("path")
 //database  
 const MONGO_URL="mongodb://127.0.0.1:27017/WanderLust";
 
@@ -16,18 +16,28 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
-app.get("/testListing",async(req,res)=>{
-    let sampleListing=new Listing({
-        title:"My new vila",
-        description:"By the beach",
-        price:1200,
-        location:"alphanche,Goa",
-        country:"India"
-    })
-    await sampleListing.save();
-    console.log("Sample was save");
-    res.send("Sucessfull testing")
-})
+//index routev completed
+app.get("/listings",async(req,res)=>{
+    const allListings=await Listing.find({})
+    res.render("listings/index",{allListings});
+    });
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"))
+
+
+// app.get("/testListing",async(req,res)=>{
+//     let sampleListing=new Listing({
+//         title:"My new vila",
+//         description:"By the beach",
+//         price:1200,
+//         location:"alphanche,Goa",
+//         country:"India"
+//     })
+//     await sampleListing.save();
+//     console.log("Sample was save");
+//     res.send("Sucessfull testing")
+// })
 
 
 app.listen(8080,()=>{
