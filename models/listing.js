b323f3cc6a6796a159d29568d63f3f59ca37f050
pratiksha950 <salunkepratiksha950@gmail.com>
2026-01-21 13:58:45 +0530

    error resolved

--- models/listing.js
+++ models/listing.js
@@ -1,28 +1,3 @@
-// const mongoose =require("mongoose")
-// const Schema=mongoose.Schema;
-
-// const listingSchema=new Schema({
-//     title:{
-//         type:String,
-//         required:true
-//     },
-//     description:String,
-//     image:{
-//         default:"https://unsplash.com/photos/a-painting-of-a-river-surrounded-by-pink-flowers-ZWg3KmVdtVw",
-//         type:String,
-//         set:(v)=>v ===""?"https://unsplash.com/photos/a-painting-of-a-river-surrounded-by-pink-flowers-ZWg3KmVdtVw":v,
-//     },
-//     price:Number,
-//     location:String,
-//     country:String
-// })
-
-// //collection
-// const Listing=mongoose.model("Listing",listingSchema);
-
-// module.exports=Listing;
-
-
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -31,40 +6,74 @@ const listingSchema = new Schema({
     type: String,
     required: true,
   },
+  description: String,
 
-  description: {
+image: {
+  url: {
     type: String,
-    required: true,
+    default:
+      "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?auto=format&fit=crop&w=800&q=60",
   },
-
-  image: {
-    filename: {
-      type: String,
-      default: "listingimage",
-    },
-    url: {
-      type: String,
-    },
-  },
-
-  price: {
-    type: Number,
-    required: true,
-    min: 0,
-  },
-
-  location: {
+  filename: {
     type: String,
-    required: true,
+    default: "listingimage",
   },
+},
 
-  country: {
-    type: String,
-    required: true,
-  },
+price: {
+  type: Number,
+  default: 0
+},
+  location: String,
+  country: String,
 });
 
-// collection
 const Listing = mongoose.model("Listing", listingSchema);
-
 module.exports = Listing;
+
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const listingSchema = new Schema({
+//   title: {
+//     type: String,
+//     required: true,
+//   },
+
+//   description: {
+//     type: String,
+//     required: true,
+//   },
+
+//   image: {
+//     filename: {
+//       type: String,
+//       default: "listingimage",
+//     },
+    
+//     url: {
+//       type: String,
+//     },
+//   },
+
+//   price: {
+//     type: Number,
+//     required: true,
+//     min: 0,
+//   },
+
+//   location: {
+//     type: String,
+//     required: true,
+//   },
+
+//   country: {
+//     type: String,
+//     required: true,
+//   },
+// });
+
+// // collection
+// const Listing = mongoose.model("Listing", listingSchema);
+
+// module.exports = Listing;
