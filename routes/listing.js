218bee00dd7338a23ca27143353728b9def7d45a
pratiksha950 <salunkepratiksha950@gmail.com>
2026-01-25 21:32:29 +0530

    middlewared added of login:

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,7 @@ const wrapAsync=require("../utils/wrapAsync.js")
 const {listingSchema}=require("../schema.js")//joi
 const ExpressError=require("../utils/ExpressError.js")
 const Listing =require("../models/Listing.js");
+const isLoggedIn =require("../views/middleware/middleware.js")
 
 
 
@@ -28,7 +29,7 @@ router.get("/",wrapAsync(async(req,res)=>{
     }));
 
 //new route
-router.get("/new",(req,res)=>{
+router.get("/new",isLoggedIn,(req,res)=>{
     res.render("listings/new")
 })
 
@@ -44,13 +45,7 @@ router.get("/:id",wrapAsync(async (req,res)=>{
 }))
 
 //create Route
-// router.post("/",validateListing, wrapAsync(async (req,res,next)=>{
-//     const newListing=new Listing(req.body.listing);
-//     await newListing.save();
-//     res.redirect("/listings")
-// }))
-
-router.post("/", validateListing, wrapAsync(async (req, res) => {
+router.post("/", validateListing,isLoggedIn, wrapAsync(async (req, res) => {
     const newListing = new Listing(req.body.listing);
 
     if (!newListing.image || !newListing.image.url) {
@@ -67,7 +62,7 @@ router.post("/", validateListing, wrapAsync(async (req, res) => {
 
 
 //Edit Route
-router.get("/:id/edit",wrapAsync(async(req,res)=>{
+router.get("/:id/edit",isLoggedIn,wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id);
         if(!listing){
@@ -78,7 +73,7 @@ router.get("/:id/edit",wrapAsync(async(req,res)=>{
 }))
 
 //update Route
-router.put("/:id",validateListing,wrapAsync(async(req,res)=>{
+router.put("/:id",validateListing,isLoggedIn,wrapAsync(async(req,res)=>{
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing})
     req.flash("success","Listing Updated !!!")
@@ -86,12 +81,14 @@ router.put("/:id",validateListing,wrapAsync(async(req,res)=>{
 }))
 
 //DELETE Route
-router.delete("/:id",wrapAsync(async(req,res)=>{
+router.delete("/:id",isLoggedIn,wrapAsync(async(req,res)=>{
     let {id}=req.params;
     let deletedListing=await Listing.findByIdAndDelete(id);
     req.flash("success","listing deleted")
     res.redirect("/listings")
 })
 )
+
+
 
 module.exports=router;
